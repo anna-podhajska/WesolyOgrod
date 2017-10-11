99c76ac00e9eddddfbb399cf93f1432695591bea
Anna Podhajska <podhajskaa@gmail.com>
2017-10-11 22:39:32 +1300

    bounce

--- client/components/HeaderBody.jsx
+++ client/components/HeaderBody.jsx
@@ -4,12 +4,12 @@ import jump from 'jump.js'
 const HeaderBody = () => (
   <div className="">
     <h1 className="title is-1" onClick={() => jump('.app-body')}>
-      <img className="HeaderBody-logo" src="logo.svg"></img> <br />
+      <img className="HeaderBody-logo" src="logo.png"></img> <br />
       <a><h1 className="HeaderBody-title">Wesoły Ogród</h1></a>
     </h1>
-    <h2 className="bounce ClickMe" onClick={() => jump('.app-body')}>
+    <h1 className="bounce animated ClickMe" onClick={() => jump('.app-body')}>
       <a>wejdź</a>
-    </h2>
+    </h1>
   </div>
 )
 
